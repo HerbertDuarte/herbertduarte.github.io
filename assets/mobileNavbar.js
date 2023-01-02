ec26a700cdcf3a776db5d5baf4effb27f33a75db
Herbert Duarte <herbertduarte.santos@gmail.com>
2023-01-02 15:02:40 -0300

    atualização do nav-menu

---
+++ assets/mobileNavbar.js
@@ -0,0 +1,52 @@
+const nav = document.querySelector('#menu')
+const btnMenu = document.querySelector('#mobile-menu')
+const menu = document.querySelector('#nav-list')
+
+function ClickBtn(event){
+
+
+  event.stopPropagation()
+  
+  //[2] Mudar o ícone quando a navbar é acionada!
+  
+  menu.classList.toggle('active')
+
+  function mudarIcone(){
+    btnMenu.classList.toggle('active')
+  }
+  mudarIcone()
+
+  //---------------[2] Fim
+
+  //[3] chamar função de click fora do range da nav
+
+  clickOut(menu, () => {
+    menu.classList.remove('active')
+    btnMenu.classList.remove('active')
+  })
+
+  //---------------[3] Fim
+}
+
+function clickOut(targetElement, callback){
+  const html = document.documentElement
+
+  function htmlClick(event){
+    if(!targetElement.contains(event.target))
+      targetElement.removeAttribute('data-target')
+      html.removeEventListener("click", htmlClick)
+      
+      callback()
+  }
+
+  if(!targetElement.hasAttribute('data-target')){
+    html.addEventListener("click", htmlClick)
+    
+    targetElement.setAttribute('data-target', '')
+  }
+}
+
+
+btnMenu.addEventListener("click", ClickBtn)
+
+
