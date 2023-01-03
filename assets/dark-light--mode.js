8ea906084abc37a4345a91a5db4f99aba4e85d2f
Herbert Duarte <herbertduarte.santos@gmail.com>
2023-01-03 02:15:44 -0300

    .

--- assets/dark-light--mode.js
+++ assets/dark-light--mode.js
@@ -1,15 +1,22 @@
 const $html = document.querySelector('html')
 const $ball = document.getElementById('menu')
+const colorbar = document.getElementsByClassName('color-bar')
 
 
 const verificarTema = ()=>{
   if(localStorage.tema == 'claro'){
     $ball.classList.add('mudar')
     $html.classList.add('light-mode')
+    colorbar[0].setAttribute('content', '#fafafa')
+    colorbar[1].setAttribute('content', '#fafafa')
+    colorbar[2].setAttribute('content', '#fafafa')  
   } 
   if(localStorage.tema == 'escuro'){
     $ball.classList.remove('mudar')
     $html.classList.remove('light-mode')
+    colorbar[0].setAttribute('content', '#2c2c2c')
+    colorbar[1].setAttribute('content', '#2c2c2c')
+    colorbar[2].setAttribute('content', '#2c2c2c') 
   }
 }
 
--- assets/dark-light-mode.js
+++ assets/dark-light-mode.js
@@ -1,11 +1,10 @@
 const $html = document.querySelector('html')
 const $checkbox = document.querySelector('.color-mode-box')
 const $ball = document.getElementById('ball')
+const colorbar = document.getElementsByClassName('color-bar')
 
 const mudarTema = ()=>{
   
-  const colorbar = document.getElementsByClassName('color-bar')
-
   if(localStorage.tema == 'escuro') {
     $html.classList.add('light-mode')
     $ball.classList.add('mudar')
@@ -26,17 +25,23 @@ const mudarTema = ()=>{
 
 const verificarTema = ()=>{
 
-if(!localStorage.tema){
-  localStorage.setItem('tema','escuro')
-}
+  if(!localStorage.tema){
+    localStorage.setItem('tema','escuro')
+  }
 
   else if(localStorage.tema == 'claro'){
     $ball.classList.add('mudar')
     $html.classList.add('light-mode')
+    colorbar[0].setAttribute('content', '#fafafa')
+    colorbar[1].setAttribute('content', '#fafafa')
+    colorbar[2].setAttribute('content', '#fafafa')  
   } 
   else if(localStorage.tema == 'escuro'){
     $ball.classList.remove('mudar')
     $html.classList.remove('light-mode')
+    colorbar[0].setAttribute('content', '#2c2c2c')
+    colorbar[1].setAttribute('content', '#2c2c2c')
+    colorbar[2].setAttribute('content', '#2c2c2c') 
   }
 }
 $checkbox.addEventListener('click', mudarTema)
