0830ea0dc86ad6df3a829a5d494de6443b6a69a6
Herbert Duarte <herbertduarte.santos@gmail.com>
2023-01-02 23:16:24 -0300

    add change mobile top bar color

--- assets/dark-light-mode.js
+++ assets/dark-light-mode.js
@@ -1,6 +1,22 @@
 const $html = document.querySelector('html')
 const $checkbox = document.querySelector('#toggle-color-mode')
 
-$checkbox.addEventListener('change', function(){
-  $html.classList.toggle('light-mode')
+
+
+$checkbox.addEventListener('click', ()=>{
+
+  const colorbar = document.getElementsByClassName('color-bar')
+  
+  if($checkbox.checked === true) {
+    $html.classList.add('light-mode')
+    colorbar[0].setAttribute('content', '#fafafa')
+    colorbar[1].setAttribute('content', '#fafafa')
+    colorbar[2].setAttribute('content', '#fafafa')  
+  }
+  if($checkbox.checked === false) {
+    $html.classList.remove('light-mode')
+    colorbar[0].setAttribute('content', '#2c2c2c')
+    colorbar[1].setAttribute('content', '#2c2c2c')
+    colorbar[2].setAttribute('content', '#2c2c2c') 
+  }
 })
