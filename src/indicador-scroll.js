5ecce69c2335130c1ffbef36db8961057ebc48f3
Herbert Duarte <herbertduarte.santos@gmail.com>
2023-11-05 21:58:35 -0300

    fix scroll indicator

--- src/indicador-scroll.js
+++ src/indicador-scroll.js
@@ -7,45 +7,122 @@
 
     /* Indicador de página pelo scrollFromTheTop */
     
-    document.addEventListener("scroll", function() {
-      var posicaoy = window.pageYOffset
+    document.addEventListener("scroll", () => {
+      var posicaoy = window.scrollY
+      const width = window.innerWidth
+      console.log(posicaoy)
       
-      if(posicaoy < 470){
-        inicio.style.color = 'var(--color02)'
-        sobre.style.color = 'var(--color04)'
-        habilidades.style.color = 'var(--color04)'
-        projetos.style.color = 'var(--color04)'
-        contatos.style.color = 'var(--color04)'
-      }
-      else if(posicaoy >= 470 && posicaoy <= 1076){
-        inicio.style.color = 'var(--color04)'
-        sobre.style.color = 'var(--color02)'
-        habilidades.style.color = 'var(--color04)'
-        projetos.style.color = 'var(--color04)'
-        contatos.style.color = 'var(--color04)'
-      }
-      else if(posicaoy >= 1076 && posicaoy <= 1750){
-        inicio.style.color = 'var(--color04)'
-        sobre.style.color = 'var(--color04)'
-        habilidades.style.color = 'var(--color02)'
-        projetos.style.color = 'var(--color04)'
-        contatos.style.color = 'var(--color04)'
-      }
-      else if(posicaoy >= 1750 && posicaoy <= 2330){
-        inicio.style.color = 'var(--color04)'
-        sobre.style.color = 'var(--color04)'
-        habilidades.style.color = 'var(--color04)'
-        projetos.style.color = 'var(--color02)'
-        contatos.style.color = 'var(--color04)'
-      }
-      else{
-        inicio.style.color = 'var(--color04)'
-        sobre.style.color = 'var(--color04)'
-        habilidades.style.color = 'var(--color04)'
-        projetos.style.color = 'var(--color04)'
-        contatos.style.color = 'var(--color02)'
-      }
+      if(width > 815){
 
+        if(posicaoy < 320){
+          inicio.style.color = 'var(--color02)'
+          sobre.style.color = 'var(--color04)'
+          habilidades.style.color = 'var(--color04)'
+          projetos.style.color = 'var(--color04)'
+          contatos.style.color = 'var(--color04)'
+        }
+        else if(posicaoy >= 320 && posicaoy <= 990){
+          inicio.style.color = 'var(--color04)'
+          sobre.style.color = 'var(--color02)'
+          habilidades.style.color = 'var(--color04)'
+          projetos.style.color = 'var(--color04)'
+          contatos.style.color = 'var(--color04)'
+        }
+        else if(posicaoy >= 990 && posicaoy <= 1560){
+          inicio.style.color = 'var(--color04)'
+          sobre.style.color = 'var(--color04)'
+          habilidades.style.color = 'var(--color02)'
+          projetos.style.color = 'var(--color04)'
+          contatos.style.color = 'var(--color04)'
+        }
+        else if(posicaoy >= 1560 && posicaoy <= 2000){
+          inicio.style.color = 'var(--color04)'
+          sobre.style.color = 'var(--color04)'
+          habilidades.style.color = 'var(--color04)'
+          projetos.style.color = 'var(--color02)'
+          contatos.style.color = 'var(--color04)'
+        }
+        else{
+          inicio.style.color = 'var(--color04)'
+          sobre.style.color = 'var(--color04)'
+          habilidades.style.color = 'var(--color04)'
+          projetos.style.color = 'var(--color04)'
+          contatos.style.color = 'var(--color02)'
+        }
+      }else if(width > 450){
+        if(posicaoy < 320){
+          inicio.style.color = 'var(--color02)'
+          sobre.style.color = 'var(--color04)'
+          habilidades.style.color = 'var(--color04)'
+          projetos.style.color = 'var(--color04)'
+          contatos.style.color = 'var(--color04)'
+        }
+        else if(posicaoy >= 320 && posicaoy <= 990){
+          inicio.style.color = 'var(--color04)'
+          sobre.style.color = 'var(--color02)'
+          habilidades.style.color = 'var(--color04)'
+          projetos.style.color = 'var(--color04)'
+          contatos.style.color = 'var(--color04)'
+        }
+        else if(posicaoy >= 990 && posicaoy <= 1560){
+          inicio.style.color = 'var(--color04)'
+          sobre.style.color = 'var(--color04)'
+          habilidades.style.color = 'var(--color02)'
+          projetos.style.color = 'var(--color04)'
+          contatos.style.color = 'var(--color04)'
+        }
+        else if(posicaoy >= 1560 && posicaoy <= 2500){
+          inicio.style.color = 'var(--color04)'
+          sobre.style.color = 'var(--color04)'
+          habilidades.style.color = 'var(--color04)'
+          projetos.style.color = 'var(--color02)'
+          contatos.style.color = 'var(--color04)'
+        }
+        else{
+          inicio.style.color = 'var(--color04)'
+          sobre.style.color = 'var(--color04)'
+          habilidades.style.color = 'var(--color04)'
+          projetos.style.color = 'var(--color04)'
+          contatos.style.color = 'var(--color02)'
+        }
+      }else {
+        if(posicaoy < 320){
+          inicio.style.color = 'var(--color02)'
+          sobre.style.color = 'var(--color04)'
+          habilidades.style.color = 'var(--color04)'
+          projetos.style.color = 'var(--color04)'
+          contatos.style.color = 'var(--color04)'
+        }
+        else if(posicaoy >= 320 && posicaoy <= 1280){
+          inicio.style.color = 'var(--color04)'
+          sobre.style.color = 'var(--color02)'
+          habilidades.style.color = 'var(--color04)'
+          projetos.style.color = 'var(--color04)'
+          contatos.style.color = 'var(--color04)'
+        }
+        else if(posicaoy >= 1280 && posicaoy <= 2100){
+          inicio.style.color = 'var(--color04)'
+          sobre.style.color = 'var(--color04)'
+          habilidades.style.color = 'var(--color02)'
+          projetos.style.color = 'var(--color04)'
+          contatos.style.color = 'var(--color04)'
+        }
+        else if(posicaoy >= 2100 && posicaoy <= 2880){
+          inicio.style.color = 'var(--color04)'
+          sobre.style.color = 'var(--color04)'
+          habilidades.style.color = 'var(--color04)'
+          projetos.style.color = 'var(--color02)'
+          contatos.style.color = 'var(--color04)'
+        }
+        else{
+          inicio.style.color = 'var(--color04)'
+          sobre.style.color = 'var(--color04)'
+          habilidades.style.color = 'var(--color04)'
+          projetos.style.color = 'var(--color04)'
+          contatos.style.color = 'var(--color02)'
+        }
+      }
+        
     })
     
     
