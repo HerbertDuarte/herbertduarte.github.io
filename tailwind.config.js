1e40a2e1a2d8d9680bd0a8193056c3be10ae6664
Herbert Duarte <herbertduarte.santos@gmail.com>
2024-05-31 20:51:23 -0300

    change primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
     extend: {
       colors: {
         'light-primary': '#ff6c6c',
-        primary: '#8b1a1a',
+        primary: '#a52121',
         secondary: '#202020',
         'dark-primary': '#570707',
       },
